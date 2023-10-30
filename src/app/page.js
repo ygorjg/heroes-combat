490b31060519715c970576e1ec03014ddcf59692
ygorjg <ygorjgr@gmail.com>
2023-10-30 12:49:40 -0300

    feat: created filter layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import Filter from "@/components/Filter";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Heroes!</h1>
+      <Filter />
     </main>
   );
 }
---
+++ src/components/Filter/index.jsx
@@ -0,0 +1,48 @@
+"use client";
+
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { withStyles } from "@mui/styles";
+import styled from "styled-components";
+
+const FilterContainer = styled.div`
+  background-color: #a13a23;
+  padding: 12px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 10px;
+`;
+
+const StyledTextField = withStyles({
+  root: {
+    backgroundColor: "#ffffffd8",
+    borderRadius: "5px",
+  },
+})(TextField);
+
+const StyledButton = withStyles({
+  root: {
+    backgroundColor: "#e35131",
+    color: "#fff",
+    height: "50px",
+    borderRadius: "5px",
+    marginLeft: "5px",
+    height: "55px",
+  },
+})(Button);
+
+const Filter = () => {
+  return (
+    <FilterContainer>
+      <StyledTextField
+        label="Digite aqui o nome do herói"
+        variant="outlined"
+        fullWidth
+      />
+      <StyledButton variant="contained">Buscar</StyledButton>
+    </FilterContainer>
+  );
+};
+
+export default Filter;
